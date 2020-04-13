58ccceded631f592f2b0daa5275df521c130fbb2
d <deidre.gerber@gmail.com>
2020-04-13 14:38:22 +0100

    Welcome Message

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Welcome from './WelcomeMessage'
+import WelcomeMessage from "./WelcomeMessage";
 
-function App() {
-  return (
-    <div className="App">
-      <div>Hello, World!</div>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <WelcomeMessage/>
+        );
+    }
 }
 
 export default App;
