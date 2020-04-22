2e1c0d1965b4627cac8784856fe2b65a74080068
d <deidre.gerber@gmail.com>
2020-04-22 19:16:38 +0100

    add styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Welcome from './WelcomeMessage'
 import WelcomeMessage from "./WelcomeMessage";
+import styled, {css} from 'styled-components';
 
+const MyButton = styled.div`
+    color: green;
+`
 
 class App extends Component {
     render() {
         return (
+            <div>
             <WelcomeMessage/>
+            <MyButton>hello</MyButton>
+        </div>
         );
+
     }
 }
 
