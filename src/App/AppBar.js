f843910ae4749b030bdbf393a35bd94e1d10f6d5
d <deidre.gerber@gmail.com>
2020-04-23 04:52:22 +0100

    Style AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
+/* Logo - CryptoStox */
+const Logo = styled.div`
+    font-size: 1.5em;
+`
+/* AppBar - Navigation Dashboard & Settings */
 const Bar = styled.div`
     display:grid;
+    margin-bottom: 40px;
     grid-template-columns: 180px auto 100px 100px;
 `
 
+/* AppBar - Active & Hover */
+const ControlButtonElem = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+    text-shadow: -1px 1px 20px #03DAC6
+    `}
+`
+
+/* AppBar - Capitalize First Letter */
+function toProperCase(lower) {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+/* AppBar - Active State */
+function ControlButton ({name, active}){
+    return (
+        <ControlButtonElem active={active}>
+            {toProperCase(name)}
+        </ControlButtonElem>
+    )
+}
+
 export default function(){
-    return <Bar>
-        <div> CryptoStox </div>
-        <div/>
-        <div> Dashboard </div>
-        <div> Settings </div>
-    </Bar>
+    return (
+        <Bar>
+            <Logo> CryptoStox </Logo>
+            <div/>
+            <ControlButton active name="dashboard"/>
+            <ControlButton name="settings"/>
+        </Bar>
+            );
 }
