75aa938f605b621fbcca31d47a909eb000531766
d <deidre.gerber@gmail.com>
2020-04-23 02:41:29 +0100

    Add App Layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from "./WelcomeMessage";
-import styled, {css} from 'styled-components';
 
-const MyButton = styled.div`
-    color: green;
-`
+import AppLayout from './AppLayout';
 
 class App extends Component {
     render() {
         return (
-            <div>
-            <WelcomeMessage/>
-            <MyButton>hello</MyButton>
-        </div>
+            <AppLayout>
+                <WelcomeMessage/>
+
+            </AppLayout>
         );
 
     }
