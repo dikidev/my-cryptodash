139f4ffc75b77f648773f09df8a26407ca095187
d <deidre.gerber@gmail.com>
2020-04-29 16:03:51 +0100

    Settings page, localStorage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -5,11 +5,35 @@ export const AppContext = React.createContext();
 export class AppProvider extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            page: 'settings',
-            setPage: this.setPage
+        this.state = { /* Default State - First Visit no data in local storage*/
+            page: 'dashboard',
+            ...this.savedSettings(),
+            setPage: this.setPage,
+            confirmFavorites: this.confirmFavorites
         }
     }
+
+    confirmFavorites = () => {
+        this.setState({
+            firstVisit: false,
+            page: 'dashboard'
+        });
+        localStorage.setItem('cryptoStox', JSON.stringify({
+            test: 'hello'
+        }));
+    }
+
+
+
+    savedSettings(){
+        let cryptoStoxData = JSON.parse(localStorage.getItem('cryptoStox'));
+        if(!cryptoStoxData) {
+            return {page: 'settings', firstVisit: true}
+            }
+        return {};
+        }
+
+
     setPage = page => this.setState({page})
 
     render(){
