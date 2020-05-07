905fccda743ac751ad81f389f223b81ac08cf3db
d <deidre.gerber@gmail.com>
2020-05-07 15:54:16 +0100

    Finish Add/Remove Coins

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,16 +1,23 @@
 import React from "react";
+import _ from 'lodash';
 
 const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
+
+const MAX_FAVORITES = 10;
 
 export class AppProvider extends React.Component {
     constructor(props) {
         super(props);
         this.state = { /* Default State - First Visit no data in local storage*/
             page: 'dashboard',
+            favorites: ['BTC', 'ETH', 'CCN', 'DOGE' ],
             ...this.savedSettings(),
             setPage: this.setPage,
+            addCoin: this.addCoin,
+            removeCoin: this.removeCoin,
+            isInFavorites: this.isInFavorites,
             confirmFavorites: this.confirmFavorites
         }
     }
@@ -24,13 +31,28 @@ export class AppProvider extends React.Component {
             this.setState({coinList});
         }
 
+        addCoin = key => {
+        let favorites = [...this.state.favorites];
+        if(favorites.length < MAX_FAVORITES){
+            favorites.push(key);
+            this.setState({favorites});
+        }
+        }
+
+        removeCoin = key => {
+            let favorites = [...this.state.favorites];
+            this.setState({favorites: _.pull(favorites, key)})
+        }
+
+        isInFavorites = key => _.includes(this.state.favorites, key)
+
     confirmFavorites = () => {
         this.setState({
             firstVisit: false,
             page: 'dashboard'
         });
         localStorage.setItem('cryptoStox', JSON.stringify({
-            test: 'hello'
+            favorites: this.state.favorites
         }));
     }
 
@@ -41,7 +63,8 @@ export class AppProvider extends React.Component {
         if(!cryptoStoxData) {
             return {page: 'settings', firstVisit: true}
             }
-        return {};
+        let {favorites} = cryptoStoxData;
+        return {favorites};
         }
 
 
