1c9df9a708521fff0927ec5e7e39b4d0a79c10ce
d <deidre.gerber@gmail.com>
2020-05-12 16:10:18 +0100

    update PriceGrid

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Page from "../Shared/Page";
+import PriceGrid from "./PriceGrid";
 
 export default function () {
     return <Page name="dashboard">
-        I'm dashboard
+        <PriceGrid/>
     </Page>
 }
