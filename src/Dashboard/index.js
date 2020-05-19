573cb1d7d3518d6b519c16b1d977232229ac2dbf
d <deidre.gerber@gmail.com>
2020-05-19 11:02:20 +0100

    Add CoinSpotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 import Page from "../Shared/Page";
 import PriceGrid from "/Users/d/Desktop/Projects/cryptodash/my-cryptodash/src/Dashboard/PriceGrid.js";
+import CoinSpotlight from "./CoinSpotlight";
 
-
+const ChartGrid = styled.div`
+    display: grid;
+    margin-top: 20px;
+    grid-gap: 15px;
+    grid-template-columns: 1fr 3fr;
+`
 
 export default function () {
     return <Page name="dashboard">
         <PriceGrid/>
+        <ChartGrid>
+            <CoinSpotlight/>
+            <div> Chart Goes Here </div>
+
+        </ChartGrid>
     </Page>
 }
